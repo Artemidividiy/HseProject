25a584e344832db902db09f1d33873c7a88e6ea5
Kasyanik <kasnikthegreat@gmail.com>
2019-10-14 19:10:16 +0300

    FLutter version(base)

--- Aimation/app/src/main/java/com/xiv/aimation/ui/home/HomeFragment.java
+++ Aimation/app/src/main/java/com/xiv/aimation/ui/home/HomeFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.xiv.aimation.R;
 
@@ -23,13 +24,8 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final RecyclerView rv = root.findViewById(R.id.rv_fragment);
+
         return root;
     }
 }
